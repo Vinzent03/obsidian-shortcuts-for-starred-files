7eeb2a3c93cb3319f65517fd155411f4587865e4
Vinzent <vinzentwitte@gmail.com>
2021-01-25 21:57:52 +0100

    - fix starred search - fix description in manifest

--- main.ts
+++ main.ts
@@ -13,8 +13,14 @@ export default class MyPlugin extends Plugin {
 	}
 	open(index: number) {
 		const items = this.app.internalPlugins.plugins.starred.instance.items;
+		console.log(items);
+
 		if (items[index]) {
-			this.app.workspace.openLinkText(items[index].path, "");
+			if (items[index].type == "file") {
+				this.app.workspace.openLinkText(items[index].path, "");
+			} else if (items[index].type == "search") {
+				new Notice(`At index ${index + 1} is a search starred, not a file`);
+			}
 		} else {
 			new Notice(`There is no starred file at index ${index + 1}`);
 		}
