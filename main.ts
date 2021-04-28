04b088b51cfceaec7410d50361bf15a2e00a7127
Vinzent <vinzentwitte@gmail.com>
2021-04-28 21:25:40 +0200

    feat: add option to change standard note mode

--- main.ts
+++ main.ts
@@ -1,13 +1,26 @@
-import { Notice, Plugin } from 'obsidian';
+import { Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 interface Item {
 	type: "file" | "search";
 	title: string,
 	path?: string,
 	query?: string,
 }
-export default class MyPlugin extends Plugin {
-	onload() {
+const DEFAULT_SETTINGS: HotkeysForStarredFilesSettings = {
+	changeStandardNoteMode: false,
+	openInPreview: false,
+};
+
+interface HotkeysForStarredFilesSettings {
+	changeStandardNoteMode: boolean;
+	openInPreview: boolean;
+}
+
+export default class HotkeysForStarredFiles extends Plugin {
+	settings: HotkeysForStarredFilesSettings;
+	async onload() {
 		console.log('loading ' + this.manifest.name);
+		await this.loadSettings();
+		this.addSettingTab(new SettingsTab(this));
 		for (let i = 1; i <= 9; i++) {
 			this.addCommand({
 				id: `open-file-${i}`,
@@ -24,7 +37,7 @@ export default class MyPlugin extends Plugin {
 		}
 	}
 	async open(index: number, inNewPane: boolean) {
-		const rawItems = this.app.internalPlugins.plugins.starred.instance.items as Item[];
+		const rawItems = (this.app as any).internalPlugins.plugins.starred.instance.items as Item[];
 		let items: Item[] = [];
 
 		for (let item of rawItems) {
@@ -41,16 +54,55 @@ export default class MyPlugin extends Plugin {
 
 		if (items[index]) {
 			if (items[index].type == "file") {
-				this.app.workspace.openLinkText(items[index].path, "", inNewPane);
+				this.app.workspace.openLinkText(items[index].path, "", inNewPane, { state: { mode: this.settings.changeStandardNoteMode ? this.settings.openInPreview ? "preview" : "source" : undefined } });
 			} else if (items[index].type == "search") {
-				const searchPlugin = this.app.internalPlugins.plugins["global-search"];
+				const searchPlugin = (this.app as any).internalPlugins.plugins["global-search"];
 				searchPlugin.instance.openGlobalSearch(items[index].query);
 			}
 		} else {
 			new Notice(`There is nothing starred at index ${index + 1}`);
 		}
 	}
+
+	async loadSettings() {
+		this.settings = Object.assign(DEFAULT_SETTINGS, await this.loadData());
+	}
+
+	async saveSettings() {
+		await this.saveData(this.settings);
+	}
+
 	onunload() {
 		console.log('unloading ' + this.manifest.name);
+	}
+}
+
+class SettingsTab extends PluginSettingTab {
+	plugin: HotkeysForStarredFiles;
+	constructor(plugin: HotkeysForStarredFiles) {
+		super(plugin.app, plugin);
+		this.plugin = plugin;
+	}
+
+	display(): void {
+		let { containerEl } = this;
+		containerEl.empty();
+		containerEl.createEl("h2", { text: this.plugin.manifest.name });
+
+		new Setting(containerEl)
+			.setName("Change standard note mode ('source' or 'preview')")
+			.setDesc("Turned off it will use default Obsidian settings.")
+			.addToggle(cb => cb.onChange(value => {
+				this.plugin.settings.changeStandardNoteMode = value;
+				this.plugin.saveSettings();
+			}).setValue(this.plugin.settings.changeStandardNoteMode));
+
+		new Setting(containerEl)
+			.setName("Open in preview mode")
+			.setDesc("This setting only takes affect, if the above setting is turned on")
+			.addToggle(cb => cb.onChange(value => {
+				this.plugin.settings.openInPreview = value;
+				this.plugin.saveSettings();
+			}).setValue(this.plugin.settings.openInPreview));
 	}
 }
